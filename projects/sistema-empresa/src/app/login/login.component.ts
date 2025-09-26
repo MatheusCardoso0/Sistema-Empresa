5e8e35c2139a8b08e8531c54f9ca2394721158c5
MatheusCardoso0 <mathde12@gmail.com>
2025-09-26 16:24:16 -0300

    Update na tela de login

--- projects/sistema-empresa/src/app/login/login.component.ts
+++ projects/sistema-empresa/src/app/login/login.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  usuario = '';
+  senha = '';
+  erro = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  login() {
+    if (this.usuario === 'admin' && this.senha === 'admin') {
+      this.erro = false;
+      alert('Login realizado com sucesso!');
+      // Redirecionar para área administrativa se desejar
+      this.fecharLogin();
+    } else {
+      this.erro = true;
+    }
   }
 
+  fecharLogin(event?: Event) {
+    if (event) event.preventDefault();
+    // Aqui você pode navegar para a home ou esconder o componente, conforme a navegação do app
+    window.history.back();
+  }
 }
